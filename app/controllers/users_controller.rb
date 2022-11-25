fad1d839c49b65a5ba10b6f179fec39aad0b294b
moringa <oscarokumu47@gmail.com>
2022-11-25 10:16:37 +0300

    Done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,10 @@ class UsersController < ApplicationController
     render json: user, include: :items
   end
 
+  private
+
+  def render_response_record_not_found
+    render json: { error: "user not found" }, status: :not_found
+  end
+
 end
